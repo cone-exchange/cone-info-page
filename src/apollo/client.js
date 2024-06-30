fccde2633dfc7260ec13d97e12baca4303c494bc
belbix <7453635@gmail.com>
2024-06-30 15:45:13 +0300

    update graph url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/cone-exchange/cone',
+    uri: 'https://api.studio.thegraph.com/query/50271/cone/version/latest',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
